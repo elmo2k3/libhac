8e0ac2f04b359559f2fb70e85257f3d55966f00b
Bjoern Biesenbach <bjoern@bjoern-b.de>
2009-01-12 22:34:09 +0100

    Beginning documentation with doxygen

--- libhac.c
+++ libhac.c
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2007-2009 Bjoern Biesenbach <bjoern@bjoern-b.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/*!
+ * \file	libhac.c
+ * \brief	main file for libhac
+ * \author	Bjoern Biesenbach <bjoern at bjoern-b dot de>
+*/
+
 /* Header files for windows */
 #ifdef _WIN32
 #warning Building win32 version
--- libhac.h
+++ libhac.h
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2007-2009 Bjoern Biesenbach <bjoern@bjoern-b.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/*!
+ * \file	libhac.h
+ * \brief	libhac header file
+ * \author	Bjoern Biesenbach <bjoern at bjoern-b dot de>
+*/
+
 #ifndef __LIBHAC_H__
 #define __LIBHAC_H__
 
@@ -34,7 +59,10 @@
 #define ADC_MODUL_3 ADC_RES*1.3
 #define ADC_MODUL_DEFAULT ADC_RES*1.25
 
-
+/*!
+ *******************************************************************************
+ * struct holding the params of one light module
+ *******************************************************************************/
 struct _rgb
 {
 	uint8_t red;
